745df298e2db92b52f44fcaff9cc4cafd989b655
ZinUru <97682387+ZinUru99@users.noreply.github.com>
2022-04-26 08:48:36 +0200

    toDo List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import { useEffect } from 'react';
+
+
+
+
 
 function App() {
+  const [data,setData] = useState(0)
+
+  
+  useEffect(()=>{
+    
+    const axios = require("axios");
+    axios
+      .get("https://gorest.co.in/public/v2/todos")
+      .then(function (response) {
+        setData(response.data);
+        console.log(response.data, "test");
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+
+  },[])
+ 
+  function strikethrough(e){
+    e.target.style.textDecoration = "line-through";
+  }
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>My To Do List</h1>
+    <label>
+      <input className='input' type="text" placeholder='Enter here'></input>
+      <button>New</button>
+    </label>
+
+
+    
+      <ul style={{listStyleType:"none"}}>
+        {data != 0 &&
+          data.map((el) => {
+            return <li onClick={strikethrough}>{el.title}</li>;
+          })}
+      </ul>
     </div>
   );
 }
+
+
 
 export default App;
